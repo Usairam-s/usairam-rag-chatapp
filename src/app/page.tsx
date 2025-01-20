c7053c6bb95486dfdddf40898715ce06ef35e142
Usairam-s <muhammaduseram@gmail.com>
2025-01-21 03:45:25 +0500

    hello

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 export default function Home() {
   return (
     <div
-      className="min-h-screen flex items-center justify-center w-full px-4
+      className="min-h-screen flex items-center justify-center w-full px-6
     "
     >
-      Append the
+      <h2 className="text-xl font-semibold">
+        https://usairam-rag-chatapp.vercel.app/put url of website here you want
+        to chat with
+      </h2>
     </div>
   );
 }

207f15a0b02adc6e5fb2d265f54fa892bd8b8860
Usairam-s <muhammaduseram@gmail.com>
2025-01-21 02:36:46 +0500

    first commit

---
+++ src/app/[...url]/page.tsx
@@ -0,0 +1,52 @@
+import { ChatWrapper } from "@/components/ChatWrapper";
+import { ragChat } from "@/lib/rag-chat";
+import { redis } from "@/lib/redis";
+import { cookies } from "next/headers";
+
+interface PageProps {
+  params: {
+    url: string | string[] | undefined;
+  };
+}
+function reconstructUrl({ url }: { url: string[] }) {
+  const decodedComponents = url.map((component) =>
+    decodeURIComponent(component)
+  );
+
+  return decodedComponents.join("/");
+}
+const page = async ({ params }: PageProps) => {
+  //@ts-ignore
+  const sessionCookie = await cookies().get("sessionId")?.value;
+
+  const reconstructedUrl = reconstructUrl({ url: params.url as string[] });
+  const sessionId = (reconstructedUrl + "--" + sessionCookie).replace(
+    /\//g,
+    ""
+  );
+
+  const isAlreadyIndexed = await redis.sismember(
+    "indexed-urls",
+    reconstructedUrl
+  );
+
+  const initialMessages = await ragChat.history.getMessages({
+    amount: 10,
+    sessionId,
+  });
+
+  if (!isAlreadyIndexed) {
+    await ragChat.context.add({
+      type: "html",
+      source: reconstructedUrl,
+      config: { chunkOverlap: 50, chunkSize: 300 },
+    });
+
+    await redis.sadd("indexed-urls", reconstructedUrl);
+  }
+  return (
+    <ChatWrapper sessionId={sessionId} initialMessages={initialMessages} />
+  );
+};
+
+export default page;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import { Providers } from "@/components/Providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,10 +26,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={cn(geistSans.className, "min-h-screen antialiased")}>
+        <Providers>
+          <main className="h-screen dark text-foreground bg-background">
+            {children}
+          </main>
+        </Providers>
       </body>
     </html>
   );
